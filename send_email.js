31258304429e4701454de38218db39b202299f38
saurang1984 <saurang1984@gmail.com>
2024-09-04 17:13:45 -0500

    Update send_email.js

--- send_email.js
+++ send_email.js
@@ -1,26 +1,56 @@
-const { EmailClient } = require('@azure/communication-email');
+const { EmailClient, KnownEmailSendStatus } = require("@azure/communication-email");
 
-const connectionString = 'endpoint=https://sp-email-comm.unitedstates.communication.azure.com/;accesskey=eJLOge0jUWFUNAKxQsembEHuZhaWR8fYrQVENBYviZ3DSeYHyfDxJQQJ99AIACULyCpJB2WqAAAAAZCStEt3';
-const recipientEmail = 'saurang1984@gmail.com';
-const subject = 'New Push to Your Repository';
-const body = 'A new push has been made to your repository.';
+const connectionString = "endpoint=https://sp-email-comm.unitedstates.communication.azure.com/;accesskey=eJLOge0jUWFUNAKxQsembEHuZhaWR8fYrQVENBYviZ3DSeYHyfDxJQQJ99AIACULyCpJB2WqAAAAAZCStEt3";
+const senderAddress = "DoNotReply@d94f4ced-31a0-47af-8e50-987478e7d3df.azurecomm.net"
+const recipientAddress = "saurang1984@gmail.com"
 
-async function sendEmail() {
-  const emailClient = new EmailClient(connectionString);
+async function main() {
+  const POLLER_WAIT_TIME = 10
 
-  const emailMessage = {
-    sender: 'DoNotReply@d94f4ced-31a0-47af-8e50-987478e7d3df.azurecomm.net',
-    recipients: [recipientEmail],
-    subject: subject,
-    body: body,
-  };
+  const message = {
+    senderAddress: senderAddress,
+    recipients: {
+      to: [{ address: recipientAddress }],
+    },
+    content: {
+      subject: "Test email from JS Sample",
+      plainText: "This is plaintext body of test email.",
+      html: "<html><h1>This is the html body of test email.</h1></html>",
+    },
+  }
 
   try {
-    const sendResult = await emailClient.send(emailMessage);
-    console.log('Email sent successfully:', sendResult);
-  } catch (error) {
-    console.error('Failed to send email:', error);
+    const client = new EmailClient(connectionString);
+
+    const poller = await client.beginSend(message);
+
+    if (!poller.getOperationState().isStarted) {
+      throw "Poller was not started."
+    }
+
+    let timeElapsed = 0;
+    while(!poller.isDone()) {
+      poller.poll();
+      console.log("Email send polling in progress");
+
+      await new Promise(resolve => setTimeout(resolve, POLLER_WAIT_TIME * 1000));
+      timeElapsed += 10;
+
+      if(timeElapsed > 18 * POLLER_WAIT_TIME) {
+        throw "Polling timed out.";
+      }
+    }
+
+    if(poller.getResult().status === KnownEmailSendStatus.Succeeded) {
+      console.log(`Successfully sent the email (operation id: ${poller.getResult().id})`);
+    }
+    else {
+      throw poller.getResult().error;
+    }
+  }
+  catch(ex) {
+    console.error(ex);
   }
 }
 
-sendEmail();
+main();
