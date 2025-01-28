e7ab23d0aa82a7842d7bd86ca3b2f983c4a18d76
saurang1984 <saurang1984@gmail.com>
2025-01-28 08:48:12 -0600

    Update and rename test-secrets.cs to test-secrets.txt

--- test-secrets.cs
+++
@@ -1,3 +0,0 @@
-Server=myserver.database.windows.net;Database=mydatabase;User Id=myuser;Password=mystrongpassword;
-DefaultEndpointsProtocol=https;AccountName=mystorageaccount;AccountKey=myaccountkey;EndpointSuffix=core.windows.net;
-12345abcd-6789-efgh-ijkl-98765mnopqrst
